6cad7a65fe1a4dae800fc7cfa6b923e14fd3c196
ArandomDev <40927866+arandomdev@users.noreply.github.com>
2023-04-23 14:35:27 -0400

    Removed unused boilerplate from axi_fft_ip.tcl

--- library/axi_fft/axi_fft_ip.tcl
+++ library/axi_fft/axi_fft_ip.tcl
@@ -3,8 +3,6 @@
 source ../../scripts/adi_env.tcl
 source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
 
-global VIVADO_IP_LIBRARY
-
 adi_ip_create axi_fft
 adi_ip_files axi_fft [list \
     "$ad_hdl_dir/library/common/up_axi.v" \
