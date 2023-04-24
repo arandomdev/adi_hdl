5dfb4d9240efed34f0436e2a5531d931a59aa709
ArandomDev <haow6449@gmail.com>
2023-04-24 17:58:37 -0400

    Changed tabs to spaces

--- library/axi_fft/axi_fft_ip.tcl
+++ library/axi_fft/axi_fft_ip.tcl
@@ -23,27 +23,27 @@ adi_add_bus "m_axis_i" "master" \
         {"m_axis_i_tvalid" "TVALID"} \
         {"m_axis_i_tlast" "TLAST"} \
         {"m_axis_i_tdata" "TDATA"} \
-	}
+    }
 
 adi_add_bus "s_axis_o" "slave" \
-	"xilinx.com:interface:axis_rtl:1.0" \
-	"xilinx.com:interface:axis:1.0" \
-	{
+    "xilinx.com:interface:axis_rtl:1.0" \
+    "xilinx.com:interface:axis:1.0" \
+    {
         {"s_axis_o_tready" "TREADY"} \
         {"s_axis_o_tvalid" "TVALID"} \
         {"s_axis_o_tlast" "TLAST"} \
         {"s_axis_o_tdata" "TDATA"} \
-	}
+    }
 
 adi_add_bus "m_axis_c" "master" \
-	"xilinx.com:interface:axis_rtl:1.0" \
-	"xilinx.com:interface:axis:1.0" \
-	{
+    "xilinx.com:interface:axis_rtl:1.0" \
+    "xilinx.com:interface:axis:1.0" \
+    {
         {"m_axis_c_tready" "TREADY"} \
         {"m_axis_c_tvalid" "TVALID"} \
         {"m_axis_c_tlast" "TLAST"} \
         {"m_axis_c_tdata" "TDATA"} \
-	}
+    }
 
 adi_add_bus_clock s_axi_aclk s_axi:m_axis_i:s_axis_o:m_axis_c s_axi_aresetn
 
